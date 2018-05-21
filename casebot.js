3a7f2709ddd4fc9e3236adae24f784159440c835
SpicyMemeCase <34421290+SpicyMemeCase@users.noreply.github.com>
2018-05-21 11:26:08 -0500

    removed extra lines

--- casebot.js
+++ casebot.js
@@ -20,28 +20,16 @@ client.on('disconnect', () => console.log('Disconnected, power off.'));
 client.on('reconnecting', () => console.log('Powering Up!'));
 
 client.on('message', async msg => { // eslint-disable-line
-
 	if (msg.author.bot) return undefined;
-
 	if (!msg.content.startsWith(PREFIX)) return undefined;
 
-
-
 	const args = msg.content.split(' ');
-
 	const searchString = args.slice(1).join(' ');
-
 	const url = args[1] ? args[1].replace(/<(.+)>/g, '$1') : '';
-
 	const serverQueue = queue.get(msg.guild.id);
 
-
-
 	let command = msg.content.toLowerCase().split(' ')[0];
-
 	command = command.slice(PREFIX.length)
-
-
 
 	if (command === 'play') {
         const voiceChannel = msg.member.voiceChannel;
@@ -55,51 +43,33 @@ client.on('message', async msg => { // eslint-disable-line
         }
 
         if (url.match(/^https?:\/\/(www.youtube.com|youtube.com)\/playlist(.*)$/)) {
-
 			const playlist = await youtube.getPlaylist(url);
-
 			const videos = await playlist.getVideos();
-
 			for (const video of Object.values(videos)) {
-
 				const video2 = await youtube.getVideoByID(video.id); // eslint-disable-line no-await-in-loop
-
 				await handleVideo(video2, msg, voiceChannel, true); // eslint-disable-line no-await-in-loop
-
 			}
-
 			return msg.channel.send(`✅ Playlist: **${playlist.title}** has been added to the queue!`);
-
 		} else {
 			try {
 				var video = await youtube.getVideo(url);
 			} catch (error) {
 				try {
 					var videos = await youtube.searchVideos(searchString, 10);
-
 					let index = 0;
-
 					msg.channel.send(`
-
 __**Song selection:**__
 
 ${videos.map(video2 => `**${++index} -** ${video2.title}`).join('\n')}
 
 Please provide a value to select one of the search results ranging from 1-10.
-
 					`);
-
 					// eslint-disable-next-line max-depth
-
 					try {
 						var response = await msg.channel.awaitMessages(msg2 => msg2.content > 0 && msg2.content < 11, {
-
 							maxMatches: 1,
-
 							time: 10000,
-
 							errors: ['time']
-
 						});
 					} catch (err) {
 						console.error(err);
@@ -166,82 +136,48 @@ ${serverQueue.songs.map(song => `**-** ${song.title}`).join('\n')}
 });
 
 async function handleVideo(video, msg, voiceChannel, playlist = false) {
-
 	const serverQueue = queue.get(msg.guild.id);
-
 	console.log(video);
-
 	const song = {
-
 		id: video.id,
-
 		title: Util.escapeMarkdown(video.title),
-
 		url: `https://www.youtube.com/watch?v=${video.id}`
-
 	};
-
 	if (!serverQueue) {
-
 		const queueConstruct = {
-
 			textChannel: msg.channel,
-
 			voiceChannel: voiceChannel,
-
 			connection: null,
-
 			songs: [],
-
 			volume: 20,
-
 			playing: true
-
 		};
-
 		queue.set(msg.guild.id, queueConstruct);
-
-
 
 		queueConstruct.songs.push(song);
 
-
-
 		try {
-
 			var connection = await voiceChannel.join();
-
 			queueConstruct.connection = connection;
-
 			play(msg.guild, queueConstruct.songs[0]);
 
 		} catch (error) {
-
 			console.error(`I could not join the voice channel: ${error}`);
-
 			queue.delete(msg.guild.id);
-
 			return msg.channel.send(`I could not join the voice channel: ${error}`);
-
+			
 		}
 
 	} else {
-
 		serverQueue.songs.push(song);
-
 		console.log(serverQueue.songs);
-
 		if (playlist) return undefined;
-
 		else return msg.channel.send(`✅ **${song.title}** has been added to the queue!`);
 
 	}
-
 	return undefined;
 
 }
-
-
 
 function play(guild, song) {
     const serverQueue = queue.get(guild.id);
