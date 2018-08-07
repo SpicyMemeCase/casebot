9a70e069964903072102ab1d723f13a15b491dd2
SpicyMemeCase <34421290+SpicyMemeCase@users.noreply.github.com>
2018-08-07 01:27:14 -0500

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-exports.BOT_TOKEN = 'NDQ1OTc5MjY3Mjc3Mzg5ODI0.DkrMwQ.F_QHNi1aIvcK9XKXJgkeFHGC91Y';
+exports.BOT_TOKEN = 'NDQ1OTc5MjY3Mjc3Mzg5ODI0.DkrNNA.kDuM8TRFXynOBx5NisN3_3wyFho';
 
 exports.PREFIX = 'c!';
 
