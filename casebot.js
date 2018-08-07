96c7521d3afbfc32050f9445ab98bb35afa8434e
SpicyMemeCase <34421290+SpicyMemeCase@users.noreply.github.com>
2018-08-06 23:24:48 -0500

    Update casebot.js

--- casebot.js
+++ casebot.js
@@ -1,5 +1,5 @@
 const { Client, Util } = require('discord.js');
-const { TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
+const { BOT_TOKEN, PREFIX, GOOGLE_API_KEY } = require('./config');
 const YouTube = require('simple-youtube-api');
 const ytdl = require('ytdl-core');
 
@@ -202,6 +202,6 @@ function play(guild, song) {
         serverQueue.textChannel.send(`Now playing: **${song.title}**`);
 }
 
-client.login(process.env.TOKEN);
+client.login(process.env.BOT_TOKEN);
 
 
